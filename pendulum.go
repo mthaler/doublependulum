daac50ae67b6085ef840aed3a0a1f08a6cdb7c85
Michael Thaler <michael.thaler2@gmail.com>
2026-04-04 10:37:14 +0200

    Updated eom.png and pendulum.go.

--- pendulum.go
+++ pendulum.go
@@ -3,14 +3,10 @@ package main
 import "math"
 
 func x(x, t, deltat float64) float64 {
-	omega := math.Sqrt(g / l)
+	omega1 := math.Sqrt(g / l1)
+	omega2 := math.Sqrt(g / l2)
 	// it is not possible to solve the equation of motion analytically, we have to solve it numerically.
 	// for x we get: x_t+1=x_t + dx / dt * Delta t
-	xdot := -omega * math.Sin(omega*t)
+	xdot := -omega1*math.Sin(omega1*t) - omega2*math.Sin(omega2*t)
 	return x + xdot + deltat
-}
-
-func xdot(t float64) float64 {
-	omega := math.Sqrt(g / l)
-	return -(x0 * omega * math.Sin(omega*t))
 }
