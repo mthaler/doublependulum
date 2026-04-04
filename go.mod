8827e8e1920bf26f1d9a9a274576e34bfd587bc6
Michael Thaler <michael.thaler2@gmail.com>
2026-04-04 09:22:35 +0200

    Updated go.mod, go.sum and main.go

--- go.mod
+++ go.mod
@@ -4,4 +4,4 @@ go 1.24.4
 
 require fyne.io/fyne v1.4.3
 
-require fyne.io/fyne/v2 v2.7.3 // indirect
+require github.com/stretchr/testify v1.11.1 // indirect
--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"math"
-
 	"fyne.io/fyne"
 )
 
@@ -19,10 +17,4 @@ func show(app fyne.App) {
 
 	window.Resize(fyne.NewSize(600, 600))
 	window.Show()
-}
-
-func calculatePos(theta1, theta2 float64) (x, y float64) {
-	xx := l1*math.Sin(theta1) + l2*math.Sin(theta2)
-	yy := l1*math.Cos(theta1) + l2*math.Cos(theta2)
-	return xx, yy
 }
