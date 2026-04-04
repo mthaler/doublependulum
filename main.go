0dcdf1d87450598a09faac186e372965dd147511
Michael Thaler <michael.thaler2@gmail.com>
2026-04-04 10:32:01 +0200

    Updated main.go.

--- main.go
+++ main.go
@@ -8,17 +8,28 @@ const (
 	x0     = 0.3
 	phimax = 0.5
 	g      = 9.81
-	l      = 4.0
+	l1     = 3.0
+	l2     = 3.0
 )
 
 func main() {
 	points := plotter.XYs{}
+	deltat := 1.0 / 100.0
 	for i := 0; i <= 500; i++ {
-		t := float64(i) / 100.0
-		points = append(points, plotter.XY{
-			X: t,
-			Y: x(t),
-		})
+		if i == 0 {
+			// as inital value for x we use x0
+			points = append(points, plotter.XY{
+				X: 0.0,
+				Y: x(x0, 0, deltat),
+			})
+		} else {
+			t := float64(i) / 100.0
+			xprev := points[i-1].X
+			points = append(points, plotter.XY{
+				X: t,
+				Y: x(xprev, t, deltat),
+			})
+		}
 	}
 	b := bounds{
 		xmin: 0,
@@ -31,23 +42,4 @@ func main() {
 		y: "x",
 	}
 	CreateLineplotPlot(points, "t - x", l, b, "eom.png")
-	points2 := plotter.XYs{}
-	for i := 0; i <= 500; i++ {
-		t := float64(i) / 100.0
-		points2 = append(points2, plotter.XY{
-			X: x(t),
-			Y: xdot(t),
-		})
-	}
-	b2 := bounds{
-		xmin: -0.3,
-		xmax: 0.3,
-		ymin: -0.5,
-		ymax: 0.5,
-	}
-	l2 := labels{
-		x: "x",
-		y: "xdot",
-	}
-	CreateLineplotPlot(points2, "x - xdot", l2, b2, "pd.png")
 }
