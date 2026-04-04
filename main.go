3a40dfefdbcd55162d0e2ef43a120a3aa6890d88
Michael Thaler <michael.thaler2@gmail.com>
2026-04-04 09:33:32 +0200

    Updated main.go.

--- main.go
+++ main.go
@@ -1,20 +1,53 @@
 package main
 
 import (
-	"fyne.io/fyne"
+	"gonum.org/v1/plot/plotter"
 )
 
 const (
-	l1 = 10.0
-	l2 = 20.0
+	x0     = 0.3
+	phimax = 0.5
+	g      = 9.81
+	l      = 4.0
 )
 
 func main() {
-}
-
-func show(app fyne.App) {
-	window := app.NewWindow("Life")
-
-	window.Resize(fyne.NewSize(600, 600))
-	window.Show()
+	points := plotter.XYs{}
+	for i := 0; i <= 500; i++ {
+		t := float64(i) / 100.0
+		points = append(points, plotter.XY{
+			X: t,
+			Y: x(t),
+		})
+	}
+	b := bounds{
+		xmin: 0,
+		xmax: 5,
+		ymin: -0.5,
+		ymax: 0.5,
+	}
+	l := labels{
+		x: "t",
+		y: "x",
+	}
+	CreateLineplotPlot(points, "t - x", l, b, "eom.png")
+	points2 := plotter.XYs{}
+	for i := 0; i <= 500; i++ {
+		t := float64(i) / 100.0
+		points2 = append(points2, plotter.XY{
+			X: x(t),
+			Y: xdot(t),
+		})
+	}
+	b2 := bounds{
+		xmin: -0.3,
+		xmax: 0.3,
+		ymin: -0.5,
+		ymax: 0.5,
+	}
+	l2 := labels{
+		x: "x",
+		y: "xdot",
+	}
+	CreateLineplotPlot(points2, "x - xdot", l2, b2, "pd.png")
 }
