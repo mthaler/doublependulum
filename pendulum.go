1fc718f5aa66efee2006b6752e84c03128be43d7
Michael Thaler <michael.thaler2@gmail.com>
2026-04-05 11:53:41 +0200

    Updated pendulum.go.

--- pendulum.go
+++ pendulum.go
@@ -7,11 +7,20 @@ func x(x, t, deltat float64) float64 {
 	omega2 := math.Sqrt(g / l2)
 	// the equation of motion is best derived using the Lagrange formalism. The Euler-Lagrane equation for the equation of motion
 	// is (d/dt)dL/d qdot_i - dL/dq_i = 0, where q_i are the generalized coordinates (in our case the angles)
-
+	// we get:
+	// (m1 + m2)l1^2 th1dotdot + m2 l1 l2 th2dotdot cos(th1-th2) + m2 l1 l2 th2dotdot sin(th1 - th2) + (m1 + m2) g l1 sin(th1) = 0
+	// and
+	// l2th2dotdot + l1th1dotdot cos(th1-th2) - l1 th1dot^2 sin(th1-th2) + g sin(th2) = 0
+	//
 	// The equation of motion is a coupled set of differential equations which I think cannot be solved analytically, thus we use
 	// the Euler method to solve it numerically.
 	// The Euler method only works for first order differential equations. The equation of motion is a a coupled set of two second
 	// order differential equations, thus we first have to transform it to first order differential equations
+	//
+	// th1 = th1dot
+	// th1 dot = [(m1+m2)l1-m2l1cos^2(th1-th2)]^-1 [-m2l1th1dot^2sin(th1-th2)cos(th1-th2)]+m2gsin(th2)cos(th1-th2)-m2l2th2dot^2sin(th1-th2-(m1+m2)gsin(th1)
+	// th2 = th2dot
+	// th2dot = [l2 - m2l2/(m1 + m2)cos^2(th1-th2)]^-1[l1 th1dot^2sin(th1-th2)-gsin(th2)+th2dot^2sin(th1-th2)cos(th1-th2)+gsin(th1)cos(th1-th2)]
 	xdot := -omega1*math.Sin(omega1*t) - omega2*math.Sin(omega2*t)
 	return x + xdot + deltat
 }
