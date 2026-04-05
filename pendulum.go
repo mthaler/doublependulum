dc557493a73380f2b1b38e16760267928a446005
Michael Thaler <michael.thaler2@gmail.com>
2026-04-05 11:58:38 +0200

    Updated pendulum.go.

--- pendulum.go
+++ pendulum.go
@@ -5,6 +5,8 @@ import "math"
 func x(x, t, deltat float64) float64 {
 	omega1 := math.Sqrt(g / l1)
 	omega2 := math.Sqrt(g / l2)
+	// The equation of motion is from this video: https://www.youtube.com/watch?v=aTMJX1ZgMB0&t=433s
+	//
 	// the equation of motion is best derived using the Lagrange formalism. The Euler-Lagrane equation for the equation of motion
 	// is (d/dt)dL/d qdot_i - dL/dq_i = 0, where q_i are the generalized coordinates (in our case the angles)
 	// we get:
