1468c64ccd7ccfe814f0cc0de613028076fbdeef
Michael Thaler <michael.thaler2@gmail.com>
2026-04-08 16:13:43 +0200

    Updated line.go.

--- line.go
+++ line.go
@@ -9,19 +9,21 @@ import (
 	"gonum.org/v1/plot/vg"
 )
 
-func CreateLineplotPlot(points plotter.XYs, title string, labels labels, bounds bounds, file string) {
+func CreateLineplotPlot(title string, labels labels, bounds bounds, file string, points ...plotter.XYs) {
 	p := plot.New()
 	p.Title.Text = title
 	p.X.Label.Text = labels.x
 	p.Y.Label.Text = labels.y
 	p.X.Min, p.X.Max, p.Y.Min, p.Y.Max = bounds.xmin, bounds.xmax, bounds.ymin, bounds.ymax
 
-	err := plotutil.AddLines(p, "", points)
-	if err != nil {
-		log.Fatalf("could not create lineplot: %+v", err)
+	for _, pts := range points {
+		err := plotutil.AddLines(p, "", pts)
+		if err != nil {
+			log.Fatalf("could not create lineplot: %+v", err)
+		}
 	}
 
-	err = p.Save(20*vg.Centimeter, 10*vg.Centimeter, file)
+	err := p.Save(20*vg.Centimeter, 10*vg.Centimeter, file)
 	if err != nil {
 		log.Fatalf("could not save lineplot: %+v", err)
 	}
