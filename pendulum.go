a7431a35a1c506a0bd427202420781bd4807994e
Michael Thaler <michael.thaler2@gmail.com>
2026-04-08 10:08:04 +0200

    Updated pendulum.go.

--- pendulum.go
+++ pendulum.go
@@ -1,5 +1,7 @@
 package main
 
+import "math"
+
 // The equation of motion is from this video: https://www.youtube.com/watch?v=aTMJX1ZgMB0&t=433s
 //
 // we make the following substituions:
@@ -17,12 +19,15 @@ package main
 
 func x(prev data, t, deltat float64) data {
 	// to simplify things we make the approximation that the previous value of a varialbe is approximately the current value of the variable
+
+	y1 := prev.y2 + deltat*prev.y2
 	d := data{}
-	d.y1 = prev.y2 + deltat*prev.y2 
-	d.y2 = ([-m2l1y2^2sin(y1-y3)cos(y1-y3)]+m2gsin(y3)cos(y1-y3)-m2ly4^2sin(y1-y3)-(m1+m2)gsin(y1))/((m1+m2)l1-m2l1cos^2(y1-y3)))
+	d.y2 = (-m2l1y2 ^ 2*sin(y1-y3)*cos(y1-y3) + m2gsin(y3)*cos(y1-y3) - m2ly4 ^ 2*sin(y1-y3) - (m1+m2)*g*sin(y1)) / ((m1+m2)*l1 - m2*l1*math.Cos(y1-prev.y3)*math.Cos(y1-prev.y3))
 
 	//y1 := prev.y1 + deltat * y2
 	//y2 := ((m1+m2)l1-m2l1cos^2(y1-y3))^-1 [-m2l1y2^2sin(y1-y3)cos(y1-y3)]+m2gsin(y3)cos(y1-y3)-m2ly4^2sin(y1-y3)-(m1+m2)gsin(y1)
 	//y3 := prev.y3 + deltat * y4
+
+	d.y2
 	return d
 }
