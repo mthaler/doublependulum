666b0220a9c7067ec2f951d3951b0273801cccbc
Michael Thaler <michael.thaler2@gmail.com>
2026-04-08 08:57:29 +0200

    Updated pendulum.go.

--- pendulum.go
+++ pendulum.go
@@ -16,9 +16,11 @@ package main
 // y4dot = [l2 - m2l2/(m1 + m2)cos^2(y1-y3)]^-1[l1 y2^2sin(y1-y3)-gsin(y3)+m2l2/(m1 +2)y4^2sin(y1-y3)cos(y1-y3)+gsin(y1)cos(y1-y3)]
 
 func x(prev data, t, deltat float64) data {
+	d := data{}
+	d.y1 = prev.y2 + deltat*prev.y2 // we make the approximation that the previous y2 is close to y2 and use the previous y2 instead of y2 to make things simpler
+
 	//y1 := prev.y1 + deltat * y2
 	//y2 := ((m1+m2)l1-m2l1cos^2(y1-y3))^-1 [-m2l1y2^2sin(y1-y3)cos(y1-y3)]+m2gsin(y3)cos(y1-y3)-m2ly4^2sin(y1-y3)-(m1+m2)gsin(y1)
 	//y3 := prev.y3 + deltat * y4
-	d := data{}
 	return d
 }
