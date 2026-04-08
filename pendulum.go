8cb929a1500a4c4c476a1b770c8b41a966360bd2
Michael Thaler <michael.thaler2@gmail.com>
2026-04-08 14:52:35 +0200

    Updated pendulum.go.

--- pendulum.go
+++ pendulum.go
@@ -2,32 +2,12 @@ package main
 
 import "math"
 
-// The equation of motion is from this video: https://www.youtube.com/watch?v=aTMJX1ZgMB0&t=433s
-//
-// we make the following substituions:
-// y1 = theta1
-// y2 = theta1dot
-// y3 = theta2
-// y4 = theta2dot
-//
-// then we get:
-//
-// y1dot = y2
-// y2dot = [(m1+m2)l1-m2l1cos^2(y1-y3)]^-1 [-m2l1y2^2sin(y1-y3)cos(y1-y3)]+m2gsin(y3)cos(y1-y3)-m2ly4^2sin(y1-y3)-(m1+m2)gsin(y1)
-// y3dot = y4
-// y4dot = [l2 - m2l2/(m1 + m2)cos^2(y1-y3)]^-1[l1 y2^2sin(y1-y3)-gsin(y3)+m2l2/(m1 +2)y4^2sin(y1-y3)cos(y1-y3)+gsin(y1)cos(y1-y3)]
-
-func x(prev data, t, deltat float64) data {
+func angles(prev data, t, deltat float64) data {
 	// to simplify things we make the approximation that the previous value of a varialbe is approximately the current value of the variable
-
 	y1 := prev.y1 + deltat*prev.y2
-
 	y2 := prev.y2 + deltat*(-m2*l1*prev.y2*prev.y2*math.Sin(y1-prev.y3)*math.Cos(y1-prev.y3)+m2*g*math.Sin(prev.y3)*math.Cos(y1-prev.y3)-m2*l2*prev.y4*prev.y4*math.Sin(y1-prev.y3)-(m1+m2)*g*math.Sin(y1))/((m1+m2)*l1-m2*l1*math.Cos(y1-prev.y3)*math.Cos(y1-prev.y3))
 	y3 := prev.y3 + deltat*prev.y4
-	y4 := (l1 * y2 * y2 * math.Sin(y1 - y3) - g * math.Sin(y3) + m2 * l2 / (m1 + m2) * prev.y4 * prev.y4 * math.Sin(y1 - y3) * math.Cos(y1 - y3)
-	//y1 := prev.y1 + deltat * y2
-	//y2 := ((m1+m2)l1-m2l1cos^2(y1-y3))^-1 [-m2l1y2^2sin(y1-y3)cos(y1-y3)]+m2gsin(y3)cos(y1-y3)-m2ly4^2sin(y1-y3)-(m1+m2)gsin(y1)
-	//y3 := prev.y3 + deltat * y4
-	d := data{}
+	y4 := prev.y4 + deltat*(l1*y2*y2*math.Sin(y1-y3)-g*math.Sin(y3)+m2*l2/(m1+m2)*prev.y4*prev.y4*math.Sin(y1-y3)*math.Cos(y1-y3)+g*math.Sin(y1)*math.Cos(y1-y3))/(l2-m2*l2/(m1+m2)*math.Cos(y1-y3)*math.Cos(y1-y3))
+	d := data{y1: y1, y2: y2, y3: y3, y4: y4}
 	return d
 }
