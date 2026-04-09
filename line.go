505dce3ec5db185ad72f41deb09bbf3c4bb0537d
Michael Thaler <michael.thaler2@gmail.com>
2026-04-09 07:09:44 +0200

    Updated eom.png and line.go.

--- line.go
+++ line.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"image/color"
 	"log"
+	"os"
 
 	"gonum.org/v1/plot"
 	"gonum.org/v1/plot/plotter"
 	"gonum.org/v1/plot/plotutil"
 	"gonum.org/v1/plot/vg"
+	"gonum.org/v1/plot/vg/draw"
+	"gonum.org/v1/plot/vg/vgimg"
 )
 
 func CreateLineplotPlot(title string, labels labels, bounds bounds, file string, lines ...*plotter.Line) {
@@ -15,6 +19,7 @@ func CreateLineplotPlot(title string, labels labels, bounds bounds, file string,
 	p.X.Label.Text = labels.x
 	p.Y.Label.Text = labels.y
 	p.X.Min, p.X.Max, p.Y.Min, p.Y.Max = bounds.xmin, bounds.xmax, bounds.ymin, bounds.ymax
+	p.BackgroundColor = color.Transparent
 
 	for _, line := range lines {
 		err := plotutil.AddLines(p, line)
@@ -23,8 +28,25 @@ func CreateLineplotPlot(title string, labels labels, bounds bounds, file string,
 		}
 	}
 
-	err := p.Save(20*vg.Centimeter, 10*vg.Centimeter, file)
+	c := vgimg.PngCanvas{vgimg.NewWith(
+		vgimg.UseWH(10*vg.Centimeter, 5*vg.Centimeter),
+		vgimg.UseBackgroundColor(color.Transparent),
+	)}
+	p.Draw(draw.New(c))
+
+	f, err := os.Create(file)
 	if err != nil {
-		log.Fatalf("could not save lineplot: %+v", err)
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	_, err = c.WriteTo(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = f.Close()
+	if err != nil {
+		log.Fatal(err)
 	}
 }
